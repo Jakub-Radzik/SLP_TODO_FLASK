88d9b9749f2db14facccbbb93fc37d309d91c49a
Jakub Radzik <jradzik4@gmail.com>
2022-04-26 19:54:26 +0200

    init with users endpoints

---
+++ app.py
@@ -0,0 +1,27 @@
+import datetime
+from flask import Flask, redirect, url_for, session
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
+from src.API.tasks import tasks
+from src.API.users import users
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.secret_key = "super secret key"
+jwt = JWTManager(app)
+app.config['JWT_SECRET_KEY'] = '5369656D696E736B69546F4A6562616E794377656C4B757277794E6965436863655A6E61634C4D414F'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=2)
+
+app.register_blueprint(users, url_prefix='/users')
+app.register_blueprint(tasks, url_prefix='/tasks')
+
+
+@app.route('/')
+def hello_world():
+    return redirect(url_for('users.login'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
---
+++ src/API/users.py
@@ -0,0 +1,76 @@
+import hashlib
+import json
+
+from flask import request, jsonify, Blueprint, render_template, session, redirect, url_for
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
+from src.DB.DB import users_collection, tasks_collection
+
+users = Blueprint('users', __name__, template_folder='templates')
+
+
+@users.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        new_user = dict(request.form)
+        # validate user here
+        new_user["password"] = hashlib.sha256(new_user["password"].encode("utf-8")).hexdigest()
+        doc = users_collection.find_one({"username": new_user["username"]})
+        if not doc:
+            users_collection.insert_one(new_user)
+            print("GITARA")
+            redirect(url_for('users.login'))
+        else:
+            print("user exists")
+    return render_template('register.html')
+
+
+@users.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login_details = dict(request.form)
+        user_from_db = users_collection.find_one({'username': login_details['login']})
+        if not user_from_db:
+            user_from_db = users_collection.find_one({'email': login_details['login']})
+        if user_from_db:
+            encrpted_password = hashlib.sha256(login_details['password'].encode("utf-8")).hexdigest()
+            if encrpted_password == user_from_db['password']:
+                nickname = user_from_db['username']
+                access_token = create_access_token(identity=nickname)
+                session['secret_key'] = access_token
+                session['username'] = nickname
+                print("GITARA")
+        print("ERROR")
+    return render_template('login.html')
+
+
+@users.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    return redirect(url_for('users.login'))
+
+
+#i dont know if we need it anymore
+@users.route("/user", methods=["GET"])
+@jwt_required()
+def profile():
+    current_user = get_jwt_identity()
+    user_from_db = users_collection.find_one({'username': current_user})
+    if user_from_db:
+        del user_from_db['_id'], user_from_db['password']
+        return jsonify({'profile': user_from_db}), 200
+    else:
+        return jsonify({'msg': 'Profile not found'}), 404
+
+
+@users.route("/api/v1/user", methods=["DELETE"])
+@jwt_required()
+def delete_user():
+    current_user = get_jwt_identity()
+    user_from_db = users_collection.find_one({'username': current_user})
+    if user_from_db:
+        users_collection.delete_one({'username': current_user})
+        tasks_collection.delete_many({'username': current_user})
+        return jsonify({'msg': 'User deleted'}), 200
+    else:
+        return jsonify({'msg': 'User not found'}), 404
+
